2652b2d599bd06236a81d3444e03478a980345f0
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-10 18:53:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,4 +9,24 @@ idxlst = []
 # ******************************
 
 # print the words that has 'a', 'r', 'e' in sequence
+for word in words:
+    sequenceCounter = 0
 
+    prevChar = ""
+    for char in word:
+        if char == are[0]:
+            # indicates the start of a sequence or resets if encounter another "a"
+            sequenceCounter = 1 
+        
+        # find other sequence if "a" has been found
+        if sequenceCounter >= 1:
+            if char == are[sequenceCounter] and char != prevChar:
+                sequenceCounter += 1
+        
+        prevChar = are[sequenceCounter - 1] 
+        
+        if sequenceCounter == 3:
+            idxlst.append(word)
+            break
+
+print(idxlst)
